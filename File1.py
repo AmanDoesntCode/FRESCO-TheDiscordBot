3ff79a0ae8dc890813509f51628ac02d606739c7
Aman Singh <98482324+AmanDoesntCode@users.noreply.github.com>
2023-02-28 16:36:59 +0530

    Added Guild detection and succesful connection establishment ping

--- File1.py
+++ File1.py
@@ -1,9 +1,17 @@
 import discord
 
 client = discord.Client(intents=discord.Intents.default())
+TOKEN = "MTA4MDA3MzcyNzQ5NDA3ODUxNA.GeNv4c.8G0jU8nLQMr3DqBolCHyAG_yqfOSiuUg-t4Mxs"
+GUILD = "ECLIPSE"
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-client.run("MTA4MDA3MzcyNzQ5NDA3ODUxNA.GeNv4c.8G0jU8nLQMr3DqBolCHyAG_yqfOSiuUg-t4Mxs")
+    print(f'{client.user} is connected to the following guild:\n'
+          f'{guild.name}(id: {guild.id})')
+
+
+client.run(TOKEN)
